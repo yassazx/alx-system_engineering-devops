24a16395c1f1fb719f97d879659932bfed9ab126
yassazx <144545118+yassazx@users.noreply.github.com>
2024-04-19 15:54:31 +0100

    commit to update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
 #!/usr/bin/pup
+
+require 'uri'
+
 # Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# Example of replacing URI.escape with URI::DEFAULT_PARSER.escape
+url = "http://example.com/path?query=value"
+escaped_url = URI::DEFAULT_PARSER.escape(url)
